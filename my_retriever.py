548729f056df9d7fec3fb098a0ab5c1c41bcc7ab
Bobby Williams <bobby.james.williams@outlook.com>
2021-11-06 14:48:58 +0000

    calculate distance added. Efficiency kinda solved

--- my_retriever.py
+++ my_retriever.py
@@ -9,9 +9,9 @@ class Retrieve:
         self.num_docs = self.compute_number_of_documents()
         if self.term_weighting == 'binary':
             print("Constructing Documents Binary Weighted Vector... ")
+            #make a dictionary of docs
             self.document_vector = self.binary_weighted_document_vectors()
 
-        
 
     def compute_number_of_documents(self):
         self.doc_ids = set()
@@ -20,58 +20,67 @@ class Retrieve:
         return len(self.doc_ids)
 
 
-    def calculate_distance(self, filtered_doc_IDs):
+    def calculate_cosine_distance(self, filtered_doc_IDs, query_vector):
+        results = {}
+        for id in filtered_doc_IDs:
+            sigma_qd = 0
+            sigma_d_sqrd = 0
 
-        pass
+            for term in self.document_vector[id]:
+                if term not in self.document_vector[id]:
+                    document_term = 0
+                else:
+                    document_term = self.document_vector[id][term]
 
-    # Method performing retrieval for a single query (which is 
+                if term not in query_vector[0]:
+                    query_term = 0
+                else:
+                    query_term = query_vector[0][term]
+
+                sigma_qd += (document_term) * (query_term)
+                sigma_d_sqrd += (document_term ** 2) ** 0.5
+
+            similarity = sigma_qd/sigma_d_sqrd
+
+            results.update({id : similarity})
+        sorted_results = {k: v for k, v in sorted(results.items(), key=lambda item: item[1], reverse= True)}
+        sorted_results
+        return sorted_results
+
+
+    # Method performing retrieval for a single query (which is
     # represented as a list of preprocessed terms). Returns list 
     # of doc ids for relevant docs (in rank order).
-    def tfidf_weighted_vector(self):
-        print("This is a test")
-        pass
 
     def binary_weighted_document_vectors(self):
-        document_vectors = []
-        for ID in self.doc_ids:
+        document_vectors = {}
+        for doc_id in self.doc_ids:
             vector = {}
             for term in self.index:
-                if ID in self.index[term]:
+                if doc_id in self.index[term]:
                     vector.update({term : 1})
-                else:
-                    vector.update({term : 0})
-            temp_dict = {'DocID': ID, 'Data' : vector}
-            document_vectors.append(temp_dict)
+            document_vectors.update({doc_id : vector})
         return document_vectors
 
     def binary_weighted_query_vector(self,query):
+        vector = {}
         for query_term in query:
-            vector = {}
-            for index_term in self.index:
-                if index_term == query_term:
-                    vector.update({index_term : 1})
-                else:
-                    vector.update({index_term : 0})
-        query_vector = ({'DocID' : 0, 'Data' : vector})
+            vector.update({query_term : 1})
+        query_vector = ({0 : vector})
         return query_vector
 
-    def construct_binary_query_vector(self,query, filtered_doc_IDs):
-        print("For " + str(query))
 
-        print("Constructing Query Binary Weighted Vector")
-        query_vector = self.binary_weighted_query_vector(query)
-        print("######################################")
-
-        pass
-
-
-
-
+### FREQUENCY WEIGHTED
 
     def frequency_weighted_vector(self):
         pass
 
     def produce_query_vector(self):
+        pass
+
+
+### TFIDF
+    def tfidf_weighted_vector(self):
         pass
 
     def calculate_similarity(self, query_vector):
@@ -95,18 +104,19 @@ class Retrieve:
                 for data in term_value:
                     docID_to_consider.append(data)
         set_of_docID_to_consider = set(docID_to_consider)
-        return set_of_docID_to_consider
+        return self.doc_ids
 
     def for_query(self, query):
+        match = []
         print("Filtering Doc IDs ...")
         filtered_doc_IDs = self.filter_documents(query)
-        self.construct_binary_query_vector(query,filtered_doc_IDs)
-        return list(range(1,11))
-
-
-
-
-
-
-
+        if self.term_weighting == 'binary':
+            print("For " + str(query))
+            print("Constructing Query Binary Weighted Vector")
+            query_vector = self.binary_weighted_query_vector(query)
+            print("######################################")
+            processed_results = self.calculate_cosine_distance(filtered_doc_IDs, query_vector)
+            return list(processed_results.keys())
+        else:
+            print("Something went wrong...")
 
