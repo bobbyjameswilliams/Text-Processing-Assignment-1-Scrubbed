ebb6314a119d04d61d63c02e901af7fe8d2e29eb
Bobby Williams <bobby.james.williams@outlook.com>
2021-11-08 17:12:36 +0000

    Assignment Completed

--- my_retriever.py
+++ my_retriever.py
@@ -9,25 +9,48 @@ class Retrieve:
         self.index = index
         self.term_weighting = term_weighting
         self.num_docs = self.compute_number_of_documents()
+        # these indexes and boolean are used in the creation of the TFIDF index.
         self.df_index = {}
         self.idf_index = {}
+        self.tf_index = {}
 
-        print("Constructing Documents Binary Weighted Vector... ")
+        # Prints to console what weighting is being produced.
+        print("Constructing Documents " + self.term_weighting.upper() + " Weighted Vector... ")
+
         # make a dictionary of docs
         if self.term_weighting == 'tfidf':
+            #TFIDF requires more global helper indexes.
             self.df_index = self.create_df_index()
             self.idf_index = self.create_idf_index()
-            self.document_vector = self.weighted_document_vectors()
-        else:
+            self.tf_index = self.weighted_document_vectors()
             self.document_vector = self.weighted_document_vectors()
 
+        else:
+            self.document_vector = self.weighted_document_vectors()
 
     def compute_number_of_documents(self):
         self.doc_ids = set()
         for term in self.index:
             self.doc_ids.update(self.index[term])
+
         return len(self.doc_ids)
 
+### MAIN FUNCTIONS ###
+
+    # Returns docIDs of documents that contain words in the query.
+    def filter_documents(self, query):
+        docID_to_consider = []
+        for term in query:
+            term_value = self.index.get(term)
+            if term_value is not None:
+                for data in term_value:
+                    docID_to_consider.append(data)
+        set_of_docID_to_consider = set(docID_to_consider)
+
+        return set_of_docID_to_consider
+
+    # Used to calculate cosine distance for all weightings. Returns the results as a sorted list of document indexs
+    # based on their similarity score.
     def calculate_cosine_distance(self, filtered_doc_IDs, query_vector):
         results = {}
         for id in filtered_doc_IDs:
@@ -37,8 +60,8 @@ class Retrieve:
             for term in self.document_vector[id]:
                 document_term = self.document_vector[id][term]
 
-                if term in query_vector[0]:
-                    query_term = query_vector[0][term]
+                if term in query_vector:
+                    query_term = query_vector[term]
                 else:
                     query_term = 0
 
@@ -49,9 +72,10 @@ class Retrieve:
 
             results.update({id: similarity})
         sorted_results = {k: v for k, v in sorted(results.items(), key=lambda item: item[1], reverse=True)}
+
         return sorted_results
 
-    # Generate the document vectors
+    # Main function for creating document vectors. Creates different ones based on the selected term weighting.
     def weighted_document_vectors(self):
         if self.term_weighting == 'binary':
             print("Producing Binary Weighted Document Vector...")
@@ -64,82 +88,73 @@ class Retrieve:
                 if doc_id in self.index[term]:
                     if self.term_weighting == 'binary':
                         vector.update({term: 1})
-                    elif self.term_weighting == 'tf':
+                    # for TFIDF, on the first pass the TF index is created as this is needed for TDFIDF
+                    elif self.term_weighting == 'tf' or (self.term_weighting == 'tfidf' and self.tf_index == {}):
                         vector.update({term: self.index[term][doc_id]})
-                    elif self.term_weighting == 'tfidf':
-                        term_df = self.df_index[term]
+                    # Once TF index is created, TFIDF index is created.
+                    elif self.term_weighting == 'tfidf' and self.tf_index != {}:
+                        term_tf = self.tf_index[doc_id][term]
                         term_idf = self.idf_index[term]
-                        tfidf = term_df * term_idf
+                        tfidf = term_tf * term_idf
                         vector.update({term: tfidf})
 
-
             document_vectors.update({doc_id: vector})
+
         return document_vectors
 
     # Method performing retrieval for a single query (which is
     # represented as a list of preprocessed terms). Returns list 
     # of doc ids for relevant docs (in rank order).
 
+    ### QUERY VECTORS ###.
+
     def binary_weighted_query_vector(self, query):
-        vector = {}
+        query_vector = {}
         for query_term in query:
-            vector.update({query_term: 1})
-        query_vector = ({0: vector})
+            query_vector.update({query_term: 1})
+
         return query_vector
 
-    ### FREQUENCY WEIGHTED
-
     def tf_query_vector(self, query):
-        vector = {}
+        query_vector = {}
         for query_term in query:
-            if query_term in vector:
-                newcount = vector[query_term] + 1
-                vector.update({query_term: newcount})
+            if query_term in query_vector:
+                newcount = query_vector[query_term] + 1
+                query_vector.update({query_term: newcount})
             else:
-                vector.update({query_term: 1})
+                query_vector.update({query_term: 1})
 
-        query_vector = ({0: vector})
         return query_vector
 
     def tfidf_query_vector(self, query):
-        vector = {}
+        tf_query_vector = self.tf_query_vector(query)
+        query_vector = {}
         for query_term in query:
             if query_term in self.index:
-                term_df = self.df_index[query_term]
+                term_tf = tf_query_vector[query_term]
                 term_idf = self.idf_index[query_term]
-                tfidf = term_df * term_idf
-                vector.update({query_term: tfidf})
-        query_vector = ({0: vector})
+                tfidf = term_tf * term_idf
+                query_vector.update({query_term: tfidf})
+
         return query_vector
 
+    ### TFIDF HELPER INDEXES ###
 
-    ### TFIDF
     def create_df_index(self):
         dictionary = {}
         for term in self.index:
             dictionary.update({term: len(self.index[term])})
         return dictionary
 
-    #Creates it in once pass at the beginning for reference later
+    # Creates it in one pass at the beginning for reference later
     def create_idf_index(self):
         dictionary = {}
         for term in self.df_index:
             df = self.df_index[term]
-            prelog_idf = (self.num_docs/df)
+            prelog_idf = (self.num_docs / df)
             idf = math.log10(prelog_idf)
             dictionary.update({term: idf})
         return dictionary
-
-    # Returns docIDs of documents that contain words in the query.
-    def filter_documents(self, query):
-        docID_to_consider = []
-        for term in query:
-            term_value = self.index.get(term)
-            if term_value is not None:
-                for data in term_value:
-                    docID_to_consider.append(data)
-        set_of_docID_to_consider = set(docID_to_consider)
-        return set_of_docID_to_consider
 
     def for_query(self, query):
         print("Filtering Doc IDs ...")
